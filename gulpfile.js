3b8c120f9f0d1952427e1b5bf05af11059fd31a0
znalbert <znalbert@gmail.com>
2016-07-17 14:30:42 -0500

    chore: Remove ESLint from gulp, reformat with JSCS
    Removed ESLint from gulpfile and reformatted it and the main.js file with linting by JSCS.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
@@ -10,82 +9,67 @@ var pngquant = require('imagemin-pngquant');
 var htmlmin = require('gulp-html-minifier');
 var inlinesource = require('gulp-inline-source');
 
-gulp.task('default', [
-	'lint'],
-	function() {
-	gulp.watch('src/css/main.css')
-		.on('change', browserSync.reload);
-	gulp.watch('src/js/main.js', ['lint'])
-		.on('change', browserSync.reload);
-	gulp.watch('src/index.html')
-		.on('change', browserSync.reload);
+gulp.task('default',
+  function () {
+    gulp.watch('src/css/main.css')
+      .on('change', browserSync.reload);
+    gulp.watch('src/js/main.js', ['lint'])
+      .on('change', browserSync.reload);
+    gulp.watch('src/index.html')
+      .on('change', browserSync.reload);
 
-	browserSync.init({
-		server: './src'
-	});
+    browserSync.init({
+      server: './src',
+    });
+  });
+
+gulp.task('copy-styles', function () {
+  gulp.src('src/css/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+    }))
+    .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('copy-styles', function() {
-	gulp.src('src/css/main.scss')
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('dist/css/'))
+gulp.task('copy-html', function () {
+  gulp.src('src/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+    }))
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('lint', function () {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-	return gulp.src(['**/*.js','!node_modules/**'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-		.pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-		.pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-		.pipe(eslint.failAfterError());
+gulp.task('copy-images', function () {
+  return gulp.src('src/**/*.{jpg,jpeg,png,gif}')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()],
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('copy-html', function() {
-	gulp.src('src/**/*.html')
-		.pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-		.pipe(inlinesource())
-		.pipe(gulp.dest('./dist'))
-});
-
-gulp.task('copy-images', function() {
-	return gulp.src('src/**/*.{jpg,jpeg,png,gif}')
-		.pipe(imagemin({
-			progressive: true,
-			use: [pngquant()]
-		}))
-		.pipe(gulp.dest('dist/'));
-});
-
-gulp.task('copy-scripts', function() {
-	gulp.src('src/js/main.js')
-		//.pipe(concat('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
+gulp.task('copy-scripts', function () {
+  gulp.src('src/js/main.js') //.pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('dist', [
-	'copy-images',
-	'copy-styles',
-	'lint',
-	'copy-scripts',
-	'copy-html']
-);
+  'copy-images',
+  'copy-styles',
+  'copy-scripts',
+  'copy-html',
+]);
 
 gulp.task('distserve', ['dist'],
-	function (){
-		browserSync.init({
-			server: './dist'
-		});
-	}
+  function () {
+    browserSync.init({
+      server: './dist',
+    });
+  }
 );
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,27 @@
 function collapseNavbar() {
-    if ($(".navbar-inverse").offset().top > 50) {
-        $(".navbar-fixed-top").removeClass("large");
-    } else {
-        $(".navbar-fixed-top").addClass("large");
-    }
+  if ($('.navbar-inverse').offset().top > 50) {
+    $('.navbar-fixed-top').removeClass('large');
+  } else {
+    $('.navbar-fixed-top').addClass('large');
+  }
 }
 
 $(window).scroll(collapseNavbar);
 
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500);
-        event.preventDefault();
-    });
+$(function () {
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top,
+    }, 1500);
+    event.preventDefault();
+  });
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+$('.navbar-collapse ul li a').click(function () {
+  if ($(this).attr('class') !=
+  'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
     $('.navbar-toggle:visible').click();
   }
 });
